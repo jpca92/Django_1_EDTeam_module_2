9835cdf1bbc6535237f57599f614862b0d1f5afc
jpca92 <jpca92@gmail.com>
2025-04-11 16:20:20 -0500

    lesson 2_4

--- todolist/urls.py
+++ todolist/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.IndexView.as_view()),
     path('task',views.TaskView.as_view()),
+    path('task/<int:pk>', views.TaskDetailView.as_view()),
     
     
 ]
--- todolist/views.py
+++ todolist/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from rest_framework.views import APIView, status
 from rest_framework.response import Response
+from django.http import Http404
 
 from .serializers import TaskSerializer
 from .models import Task
@@ -27,3 +28,27 @@ class TaskView(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+class TaskDetailView(APIView):
+    def get_object (self, pk):
+        try:
+            return Task.objects.get(pk=pk)
+        except Task.DoesNotExist:
+            raise Http404
+    
+    def get(self, request, pk):
+        task = self.get_object(pk)
+        serializer = TaskSerializer(task)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    def put(self, request, pk):
+        task = self.get_object(pk)
+        serializer = TaskSerializer(task, data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def delete(self, request, pk):
+        task= self.object(pk)
+        task.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
